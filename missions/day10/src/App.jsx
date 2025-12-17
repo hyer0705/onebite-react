c26d3cf6c2247c6af849bc31d1f323781a04f737
조혜린 <hyer1205@gmail.com>
2025-12-17 14:34:28 +0900

    Feat: Day 10 Welcome React App 만들기

--- missions/day10/src/App.jsx
+++ missions/day10/src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.css";
 
+function Welcome({ name, isMember }) {
+  if (isMember) {
+    return <h1>{name}님, 다시 오셨군요.</h1>;
+  }
+
+  return <h1>{name}님, 가입하시겠어요?</h1>;
+}
+
 function App() {
   return (
     <>
-      <h1>Hello, World!</h1>
+      <Welcome name="Lucy" isMember={false} />
     </>
   );
 }
