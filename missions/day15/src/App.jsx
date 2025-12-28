7754f4dab70d8cfb7a2f955021e31a58aa48000e
조혜린 <hyer1205@gmail.com>
2025-12-28 12:37:25 +0900

    Feat: 연락처 추가하기 구현

--- missions/day15/src/App.jsx
+++ missions/day15/src/App.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useRef } from "react";
 import "./App.css";
 import ContactEditor from "./components/ContactEditor";
 import ContactList from "./components/ContactList";
@@ -16,16 +16,34 @@ const mockContacts = [
   },
 ];
 
-function reducer() {}
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE_CONTACT":
+      return [action.data, ...state];
+      break;
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const contactId = useRef(3);
   const [contacts, dispatch] = useReducer(reducer, mockContacts);
+
+  const onCreate = (data) => {
+    const newContact = {
+      ...data,
+      id: contactId.current++,
+    };
+
+    dispatch({ type: "CREATE_CONTACT", data: newContact });
+  };
 
   return (
     <div className="App">
       <h2>Contact List</h2>
       <section>
-        <ContactEditor />
+        <ContactEditor onCreate={onCreate} />
       </section>
       <section>
         <ContactList contacts={contacts} />
--- missions/day15/src/components/ContactEditor.jsx
+++ missions/day15/src/components/ContactEditor.jsx
@@ -1,14 +1,48 @@
+import { useReducer } from "react";
 import "./ContactEditor.css";
 
-export default function ContactEditor() {
+export default function ContactEditor({ onCreate }) {
+  const [contact, dispatch] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case "CHANGE_NAME":
+          return { ...state, name: action.data };
+        case "CHANGE_EMAIL":
+          return { ...state, email: action.data };
+        case "RESET":
+          return { name: "", email: "" };
+        default:
+          return state;
+      }
+    },
+    { name: "", email: "" }
+  );
+
   return (
     <div className="ContactEditor">
       <div className="title">Add Contact</div>
       <div className="input_wrapper">
-        <input className="name" placeholder="이름 ..." />
-        <input className="contact" placeholder="연락처(이메일) ..." />
+        <input
+          value={contact.name}
+          onChange={(e) => dispatch({ type: "CHANGE_NAME", data: e.target.value })}
+          className="name"
+          placeholder="이름 ..."
+        />
+        <input
+          value={contact.email}
+          onChange={(e) => dispatch({ type: "CHANGE_EMAIL", data: e.target.value })}
+          className="contact"
+          placeholder="연락처(이메일) ..."
+        />
       </div>
-      <button>Add</button>
+      <button
+        onClick={() => {
+          onCreate(contact);
+          dispatch({ type: "RESET" });
+        }}
+      >
+        Add
+      </button>
     </div>
   );
 }
