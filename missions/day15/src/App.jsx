abf1de7ef1057bb61d5569895898a028f887710c
조혜린 <hyer1205@gmail.com>
2025-12-28 12:24:48 +0900

    Feat: 연락처 목록 조회 구현

--- missions/day15/src/App.jsx
+++ missions/day15/src/App.jsx
@@ -1,8 +1,26 @@
+import { useReducer } from "react";
 import "./App.css";
 import ContactEditor from "./components/ContactEditor";
 import ContactList from "./components/ContactList";
 
+const mockContacts = [
+  {
+    id: 1,
+    name: "한입스튜디오",
+    email: "onebite.fe@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Lucy",
+    email: "lucy.dev@gmail.com",
+  },
+];
+
+function reducer() {}
+
 function App() {
+  const [contacts, dispatch] = useReducer(reducer, mockContacts);
+
   return (
     <div className="App">
       <h2>Contact List</h2>
@@ -10,7 +28,7 @@ function App() {
         <ContactEditor />
       </section>
       <section>
-        <ContactList />
+        <ContactList contacts={contacts} />
       </section>
     </div>
   );
--- missions/day15/src/components/ContactItem.jsx
+++ missions/day15/src/components/ContactItem.jsx
@@ -1,10 +1,10 @@
 import "./ContactItem.css";
 
-export default function ContactItem() {
+export default function ContactItem({ id, name, email }) {
   return (
     <div className="ContactItem">
-      <div className="name">이정환</div>
-      <div className="contact">king199777@gmail.com</div>
+      <div className="name">{name}</div>
+      <div className="contact">{email}</div>
       <button>🗑️ Remove</button>
     </div>
   );
