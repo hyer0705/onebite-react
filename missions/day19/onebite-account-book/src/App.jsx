14f635362c97a2fc6e59d7d83d325b34b134c084
조혜린 <hyer1205@gmail.com>
2025-12-30 14:23:58 +0900

    Feat: day 19 mission 한입 가계부 앱 환경 설정
    - day 18 mission copy & paste

---
+++ missions/day19/onebite-account-book/src/App.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router";
+import "./App.css";
+import Home from "./pages/Home";
+import NewTransaction from "./pages/NewTransaction";
+import EditTransaction from "./pages/EditTransaction";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new-transaction" element={<NewTransaction />} />
+        <Route path="/edit-transaction/:id" element={<EditTransaction />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
