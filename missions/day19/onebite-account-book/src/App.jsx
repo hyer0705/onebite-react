ea8471664f31ba6cb623fe81e7922c5d854e4ec1
조혜린 <hyer1205@gmail.com>
2025-12-30 14:50:59 +0900

    Feat: 요구사항 2 수행

--- missions/day19/onebite-account-book/src/App.jsx
+++ missions/day19/onebite-account-book/src/App.jsx
@@ -3,8 +3,63 @@ import "./App.css";
 import Home from "./pages/Home";
 import NewTransaction from "./pages/NewTransaction";
 import EditTransaction from "./pages/EditTransaction";
+import { useReducer, useRef } from "react";
+import { mockData } from "./data/transactionMockData";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INIT":
+      break;
+    case "CREATE":
+      return [action.transaction, ...state];
+    case "UPDATE":
+      return state.map((transaction) => (transaction.id == action.transaction.id ? action.transaction : transaction));
+    case "DELETE":
+      return state.filter((transaction) => transaction.id != action.id);
+  }
+
+  return state;
+};
 
 function App() {
+  const [transactions, setTransactions] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
+
+  const onCreateTransaction = (name, amount, type, category, date) => {
+    setTransactions({
+      type: "CREATE",
+      transaction: {
+        id: idRef.current++,
+        name,
+        amount,
+        type,
+        category,
+        date,
+      },
+    });
+  };
+
+  const onUpdateTransaction = (id, name, amount, type, category, date) => {
+    setTransactions({
+      type: "UPDATE",
+      transaction: {
+        id,
+        name,
+        amount,
+        type,
+        category,
+        date,
+      },
+    });
+  };
+
+  const onDeleteTransaction = (id) => {
+    setTransactions({
+      type: "DELETE",
+      id,
+    });
+  };
+
   return (
     <>
       <Routes>
