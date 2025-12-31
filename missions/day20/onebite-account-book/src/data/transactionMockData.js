71f0150a453308270a1d069b768e3e901af16a34
조혜린 <hyer1205@gmail.com>
2025-12-31 21:44:27 +0900

    Chore: 한입 챌린지 React Day 20 미션 환경 설정

---
+++ missions/day20/onebite-account-book/src/data/transactionMockData.js
@@ -0,0 +1,26 @@
+export const mockData = [
+  {
+    id: 0,
+    name: "마라탕 & 꿔바로우",
+    amount: 59000,
+    type: "expense",
+    category: "🍚 식비",
+    date: new Date().getTime() + 1,
+  },
+  {
+    id: 1,
+    name: "월세",
+    amount: 500000,
+    type: "expense",
+    category: "🏠 생활",
+    date: new Date().getTime() + 2,
+  },
+  {
+    id: 2,
+    name: "월급",
+    amount: 3500000,
+    type: "income",
+    category: "🏢 급여",
+    date: new Date().getTime() + 3,
+  },
+];
