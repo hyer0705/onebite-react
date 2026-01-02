8c211ab96985e19eda24b6a0f560eb39b1be9054
조혜린 <hyer1205@gmail.com>
2026-01-02 17:07:31 +0900

    Feat: 작성하기 버튼 클릭 이벤트 핸들러 구현
    - 작성하기 버튼 클릭시 '/new-transaction' 라우트로 이동

--- missions/day21/onebite-account-book/src/pages/Home.jsx
+++ missions/day21/onebite-account-book/src/pages/Home.jsx
@@ -1,21 +1,27 @@
 import "./Home.css";
 import { useTransactionState } from "../context/TransactionContext";
 import TransactionItem from "../components/TransactionItem";
+import { useNavigate } from "react-router";
 
 const Home = () => {
-  const transactions = useTransactionState();
+  const nav = useNavigate();
 
+  const transactions = useTransactionState();
   const getSorted = () => {
     return transactions.toSorted((a, b) => b.date - a.date);
   };
 
   const sortedTransactions = getSorted();
 
+  const onCreateButtonClick = () => {
+    nav("/new-transaction");
+  };
+
   return (
     <section className="home">
       <header>
         <h1>한입 가계부</h1>
-        <button>+ 작성하기</button>
+        <button onClick={onCreateButtonClick}>+ 작성하기</button>
       </header>
       <div className="home_transactions">
         {sortedTransactions.map((transaction) => (
