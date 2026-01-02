59079fdc2f63468215672ae10bac22ec7810d1d1
Lucy <hyer1205@gmail.com>
2026-01-02 18:49:14 +0900

    [Day 21] Mission: 한입 가계부 앱 New 페이지 구현하기 (#4)
    * Chore: 한입 가계부 챌린지 React Day 21 미션 환경 설정
    
    * Feat: 작성하기 버튼 클릭 이벤트 핸들러 구현
    
    - 작성하기 버튼 클릭시 '/new-transaction' 라우트로 이동
    
    * Feat: TransactionEditor 컴포넌트 구현
    
    * Feat: TransactionEditor 컴포넌트 기능 구현
    
    - 취소버튼 클릭 이벤트 핸들러
    - 저장버튼 클릭 이벤트 핸들러
    - useState() 훅을 사용하여 TransactionEditor 상태 관리
    
    * Feat: NewTransaction 컴포넌트 UI 구현
    
    * Refactor: categories 변수 상수로 관리 및 constants 폴더로 이동
    
    * Refactor: transaction type 상수로 관리

---
+++ missions/day21/onebite-account-book/src/components/TransactionEditor.jsx
@@ -0,0 +1,112 @@
+import { useState, useRef } from "react";
+import { useNavigate } from "react-router";
+import "./TransactionEditor.css";
+import { CATEGORIES } from "../constants/categories";
+import { TRANSACTION_TYPES } from "../constants/transaction";
+
+const TransactionEditor = ({ onCreate }) => {
+  const nav = useNavigate();
+  const typeRef = useRef();
+  const nameRef = useRef();
+  const amountRef = useRef();
+  const categoryRef = useRef();
+  const inputDateRef = useRef();
+
+  const [transactionInput, setTransactionInput] = useState({
+    type: TRANSACTION_TYPES.EXPENSE,
+    name: "",
+    amount: "",
+    category: "food",
+    date: "",
+  });
+
+  const onChange = (e) => {
+    setTransactionInput((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSaveButtonClick = () => {
+    const { type, name, amount, category, date } = transactionInput;
+
+    if (type === "") {
+      typeRef.current.focus();
+      return;
+    } else if (name === "") {
+      nameRef.current.focus();
+      return;
+    } else if (amount === "" || amount === 0) {
+      amountRef.current.focus();
+      return;
+    } else if (category === "") {
+      categoryRef.current.focus();
+      return;
+    } else if (date === "") {
+      inputDateRef.current.focus();
+      return;
+    }
+
+    onCreate(name, amount, type, CATEGORIES.get(category), new Date(date).getTime());
+    nav("/", { replace: true });
+  };
+
+  const onCancelButtonClick = () => {
+    nav("/", { replace: true });
+  };
+
+  const onInputDateClick = () => {
+    inputDateRef.current.showPicker();
+  };
+
+  return (
+    <div className="transaction_editor">
+      <section className="type_section input_wrapper">
+        <label>분류</label>
+        <select ref={typeRef} value={transactionInput.type} name="type" onChange={onChange}>
+          <option value={TRANSACTION_TYPES.EXPENSE}>지출</option>
+          <option value={TRANSACTION_TYPES.INCOME}>수입</option>
+        </select>
+      </section>
+      <section className="name_section input_wrapper">
+        <label>지출/수입 이름</label>
+        <input
+          ref={nameRef}
+          value={transactionInput.name}
+          name="name"
+          onChange={onChange}
+          type="text"
+          placeholder="지출 & 수입 이름을 입력하세요..."
+        />
+      </section>
+      <section className="amount_section input_wrapper">
+        <label>지출/수입 금액</label>
+        <input ref={amountRef} value={transactionInput.amount} name="amount" onChange={onChange} type="number" placeholder="금액을 입력하세요" />
+      </section>
+      <section className="category_section input_wrapper">
+        <label>카테고리</label>
+        <select ref={categoryRef} value={transactionInput.category} name="category" onChange={onChange}>
+          {[...CATEGORIES].map(([key, value], i) => (
+            <option key={`${i}_${key}`} value={key}>
+              {value}
+            </option>
+          ))}
+        </select>
+      </section>
+      <section className="date_section input_wrapper">
+        <label>날짜</label>
+        <input onClick={onInputDateClick} ref={inputDateRef} type="date" value={transactionInput.date} name="date" onChange={onChange} />
+      </section>
+      <section className="button_section">
+        <button className="save_button" onClick={onSaveButtonClick}>
+          저장
+        </button>
+        <button className="cancel_button" onClick={onCancelButtonClick}>
+          취소
+        </button>
+      </section>
+    </div>
+  );
+};
+
+export default TransactionEditor;
---
+++ missions/day21/onebite-account-book/src/constants/categories.js
@@ -0,0 +1,7 @@
+export const CATEGORIES = new Map([
+  ["food", "🍚 식비"],
+  ["subscribe", "💧 구독"],
+  ["life", "🏠 생활"],
+  ["salary", "🏢 급여"],
+  ["finance", "💰 금융"],
+]);
